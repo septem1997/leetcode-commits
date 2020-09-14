4b4c396ae4be7ef6fce025624e364ea2d8de11ae
septem1997 <750627595@qq.com>
2020-09-14 21:08:15 +0800

    first commit

---
+++ [94]二叉树的中序遍历.js
@@ -0,0 +1,43 @@
+//给定一个二叉树，返回它的中序 遍历。 
+//
+// 示例: 
+//
+// 输入: [1,null,2,3]
+//   1
+//    \
+//     2
+//    /
+//   3
+//
+//输出: [1,3,2] 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树 哈希表 
+// 👍 702 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    let res = []
+    inorder(root,res)
+    return res
+};
+var inorder = function(root,res){
+    if(root){
+        inorder(root.left,res)
+        res.push(root.val)
+        inorder(root.right,res)
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
