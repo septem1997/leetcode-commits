ec34dfc48c254f9b81f52c8046ae11f450851142
septem1997 <750627595@qq.com>
2020-09-28 19:37:02 +0800

    两个数组的交集

---
+++ [349]两个数组的交集.js
@@ -0,0 +1,46 @@
+//给定两个数组，编写一个函数来计算它们的交集。
+//
+//
+//
+// 示例 1：
+//
+// 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+//输出：[2]
+//
+//
+// 示例 2：
+//
+// 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//输出：[9,4]
+//
+//
+//
+// 说明：
+//
+//
+// 输出结果中的每个元素一定是唯一的。
+// 我们可以不考虑输出结果的顺序。
+//
+// Related Topics 排序 哈希表 双指针 二分查找
+// 👍 234 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const res = new Set(),set  = new Set()
+    for (const n of nums1){
+        set.add(n)
+    }
+    for (const n of nums2){
+        if (set.has(n)){
+            res.add(n)
+        }
+    }
+    return Array.from(res)
+};
+//leetcode submit region end(Prohibit modification and deletion)
