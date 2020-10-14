f56346873ae1208dd310e262b939bed5fc404aad
septem1997 <750627595@qq.com>
2020-10-14 13:59:02 +0800

    有效的数独

--- [36]有效的数独.js
+++ [36]有效的数独.js
@@ -74,7 +74,7 @@ var isValidSudoku = function(board) {
     for(let i = 0;i<9;i++){
         for(let j = 0;j<9;j++) {
             const num = board[i][j]
-            if (isNaN(num)){
+            if (num==='.'){
                 continue
             }
             const block = Math.floor(i/3)*3+Math.floor(j/3)
