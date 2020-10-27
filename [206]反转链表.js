8f279a9258da27a16ab38a76e333960e52bdb133
septem1997 <750627595@qq.com>
2020-10-27 13:58:45 +0800

    反转链表

---
+++ [206]反转链表.js
@@ -0,0 +1,38 @@
+//反转一个单链表。
+//
+// 示例:
+//
+// 输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL
+//
+// 进阶:
+//你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+// Related Topics 链表
+// 👍 1303 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head,pre) {
+    if (head===null){
+        return pre||null
+    }
+    const temp = head.next
+    if (pre){
+        head.next = pre
+    }else {
+        head.next = null
+    }
+    return reverseList(temp,head)
+};
+//leetcode submit region end(Prohibit modification and deletion)
