f6786544005c08a122c46afda7bf656d834c5e42
alinlinbo <750627595@qq.com>
2021-04-12 13:52:53 +0800

    相同的树

---
+++ [100]相同的树.js
@@ -0,0 +1,70 @@
+//给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+//
+// 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：p = [1,2,3], q = [1,2,3]
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：p = [1,2], q = [1,null,2]
+//输出：false
+//
+//
+// 示例 3：
+//
+//
+//输入：p = [1,2,1], q = [1,1,2]
+//输出：false
+//
+//
+//
+//
+// 提示：
+//
+//
+// 两棵树上的节点数目都在范围 [0, 100] 内
+// -104 <= Node.val <= 104
+//
+// Related Topics 树 深度优先搜索
+// 👍 598 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+    const leftP = [], leftQ = []
+    const pushNode = function (root,leftArr) {
+        if (!root){
+            leftArr.push(null)
+            return
+        }
+        leftArr.push(root.val)
+        pushNode(root.left,leftArr)
+        pushNode(root.right,leftArr)
+    }
+    pushNode(p,leftP)
+    pushNode(q,leftQ)
+    return leftP.toString()===leftQ.toString()
+
+};
+//leetcode submit region end(Prohibit modification and deletion)
